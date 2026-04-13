fd682b3b4b72eb69c8c1b530e1d6ca43c615b863
Manu <mries444@gmail.com>
2026-04-13 20:58:37 +0200

    delete files

--- src/data/informatik_leaf_pages.ts
+++
@@ -1,7 +0,0 @@
-import { createLegacyLeafPageStore } from "./legacyLeafPageFactory";
-
-export const informatikLeafPages = createLegacyLeafPageStore({
-  subjectTitle: "Informatik",
-  mascot: "/assets/mascots/hacker.png",
-  tonePrefix: "cs",
-});
